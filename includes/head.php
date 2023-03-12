53a144e656e129638e8fadcd711c1344ea4ef0a5
Mae <fortunemaeva2001@gmail.com>
2023-03-12 23:52:27 +0100

    add fr

--- includes/head.php
+++ includes/head.php
@@ -21,7 +21,7 @@
     <script type="text/javascript" src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.13.3/js/dataTables.bootstrap5.min.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.3/css/jquery.dataTables.min.css">
-    <script src="bootstrap-table-fr-FR.js"></script>
+    <script src="../js/bootstrap-table-fr-FR.js"></script>
 
     <!-- Table sort -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.3/css/jquery.dataTables.min.css">
