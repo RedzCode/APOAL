d35de0f7e26d9c9427fbc7fe486dfa2c89ad0fc4
Mae <fortunemaeva2001@gmail.com>
2023-03-12 23:36:07 +0100

    add style

--- includes/head.php
+++ includes/head.php
@@ -10,16 +10,19 @@
     <link href="../style/exchange.css" rel="stylesheet">
 
     <!-- Bootstrap-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
 
     <!-- Bootstrap table -->
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.13.3/js/dataTables.bootstrap5.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.3/css/jquery.dataTables.min.css">
+
+    <!-- Table sort -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.3/css/jquery.dataTables.min.css">
+    <script type="text/javascript" src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js"></script>
 
     <!-- Tab name -->
     <title>Apoal
--- screens/test.php
+++ screens/test.php
@@ -16,7 +16,7 @@ require_once("../includes/head.php") ?>
     <h1 class="pb-2">Liste des joueurs</h1>
 
     <div class="table-responsive" id="no-more-tables">
-        <table class="table table-bordered  bg-white  table-striped" id="player-table" data-toggle="table" data-locale="fr-FR" data-search="true" data-pagination="true" data-page-list="[10, 25, 50, 100, all]">
+        <table class="table table-bordered  bg-white  table-striped display" id="player-table" data-toggle="table" data-locale="fr-FR" data-search="true" data-pagination="true" data-page-list="[5, 25, 50, 100, all]">
             <thead class="bg-dark text-light">
                 <tr>
                     <th data-sortable="true">#</th>
@@ -27,6 +27,41 @@ require_once("../includes/head.php") ?>
                 </tr>
             </thead>
             <tbody>
+                <tr>
+                    <td data-title="#">1</td>
+                    <td data-title="First Name">Clark</td>
+                    <td data-title="Last Name">Kent</td>
+                    <td data-title="Email">clarkkent@mail.com</td>
+                    <td data-title="Biography">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                </tr>
+                <tr>
+                    <td data-title="#">1</td>
+                    <td data-title="First Name">Clark</td>
+                    <td data-title="Last Name">Kent</td>
+                    <td data-title="Email">clarkkent@mail.com</td>
+                    <td data-title="Biography">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                </tr>
+                <tr>
+                    <td data-title="#">1</td>
+                    <td data-title="First Name">Clark</td>
+                    <td data-title="Last Name">Kent</td>
+                    <td data-title="Email">clarkkent@mail.com</td>
+                    <td data-title="Biography">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                </tr>
+                <tr>
+                    <td data-title="#">1</td>
+                    <td data-title="First Name">Clark</td>
+                    <td data-title="Last Name">Kent</td>
+                    <td data-title="Email">clarkkent@mail.com</td>
+                    <td data-title="Biography">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                </tr>
+                <tr>
+                    <td data-title="#">1</td>
+                    <td data-title="First Name">Clark</td>
+                    <td data-title="Last Name">Kent</td>
+                    <td data-title="Email">clarkkent@mail.com</td>
+                    <td data-title="Biography">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                </tr>
                 <tr>
                     <td data-title="#">1</td>
                     <td data-title="First Name">Clark</td>
