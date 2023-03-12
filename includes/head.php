c72befafed702a8c622343c04cd4837a03f23711
Mae <fortunemaeva2001@gmail.com>
2023-03-12 17:30:12 +0100

    test page

--- includes/head.php
+++ includes/head.php
@@ -10,16 +10,15 @@
     <link href="../style/exchange.css" rel="stylesheet">
 
     <!-- Bootstrap-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
 
     <!-- Bootstrap table -->
 
-    <link href="https://unpkg.com/bootstrap-table@1.21.3/dist/bootstrap-table.min.css" rel="stylesheet">
-    <script src="https://unpkg.com/bootstrap-table@1.21.3/dist/bootstrap-table.min.js"></script>
-    <script src="https://unpkg.com/bootstrap-table@1.21.3/dist/extensions/mobile/bootstrap-table-mobile.min.js">
-    </script>
+
 
 
     <!-- Tab name -->
--- screens/test.php
+++ screens/test.php
@@ -14,21 +14,46 @@ require_once("../includes/head.php") ?>
 
 <body>
     <h1>Test</h1>
-    <table id="table" data-show-columns="true" data-search="true" data-url="json/data1.json" data-mobile-responsive="true" data-check-on-init="true">
-        <thead>
-            <tr>
-                <th data-field="id" data-sortable="true">ID</th>
-                <th data-field="name" data-sortable="true">Item Name</th>
-                <th data-field="price" data-sortable="true">Item Price</th>
-            </tr>
-        </thead>
-    </table>
-
-    <script>
-        $(function() {
-            $('#table').bootstrapTable()
-        })
-    </script>
+    <div class="m-4">
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Biography</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Clark</td>
+                        <td>Kent</td>
+                        <td>clarkkent@mail.com</td>
+                        <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>John</td>
+                        <td>Carter</td>
+                        <td>johncarter@mail.com</td>
+                        <td>Vestibulum consectetur scelerisque bibendum scelerisque purus.</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Peter</td>
+                        <td>Parker</td>
+                        <td>peterparker@mail.com</td>
+                        <td>Integer pulvinar leo id risus interdum vel metus dignissim.</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <p class="mt-4"><strong>Note:</strong> Change the editor layout/orientation to see how responsive table works.
+        </p>
+    </div>
 </body>
 
 </html>
