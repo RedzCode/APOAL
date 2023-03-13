72a68efb32efc161e16be4b4c47f34ae99aae3ea
Mae <fortunemaeva2001@gmail.com>
2023-03-13 21:46:18 +0100

    update player

--- screens/players.php
+++ screens/players.php
@@ -18,10 +18,12 @@ require_once("../includes/head.php") ?>
         <h1 class="pb-2">Liste des joueurs</h1>
         <section>
             <div class="table-responsive" id="no-more-tables">
-                <table class="table table-bordered  bg-white  table-striped display" id="player-table" data-toggle="table" data-locale="fr-FR" data-search="true" data-pagination="true" data-page-list="[5, 25, 50, 100, all]">
+                <table class="table table-bordered  bg-white  table-striped display" id="player-table"
+                    data-toggle="table" data-locale="fr-FR" data-search="true" data-pagination="true"
+                    data-page-list="[5, 25, 50, 100, all]">
                     <thead class="bg-dark text-light">
                         <tr>
-                            <th data-sortable="true">&#1F381</th>
+                            <th data-sortable="true"><?= "\u{1F441}" ?></th>
                             <th data-sortable="true">Prénom</th>
                             <th data-sortable="true">Nom</th>
                             <th data-sortable="true">Email</th>
@@ -31,13 +33,13 @@ require_once("../includes/head.php") ?>
                     <tbody>
                         <?php
                         foreach ($players as $player) { ?>
-                            <tr>
-                                <td class="number-box" data-title="#"><?= $player['NumBox'] ?> </td>
-                                <td data-title="First Name"> <?= $player['Name'] ?> </td>
-                                <td data-title="Last Name"><?= $player['FamilyName'] ?></td>
-                                <td data-title="Email"><?= $player['Email'] ?></td>
-                                <td data-title="Nb exchange"><?= $player['CountExchange'] ?></td>
-                            </tr>
+                        <tr>
+                            <td class="number-box" data-title="#"><?= $player['NumBox'] ?> </td>
+                            <td data-title="First Name"> <?= $player['Name'] ?> </td>
+                            <td data-title="Last Name"><?= $player['FamilyName'] ?></td>
+                            <td data-title="Email"><?= $player['Email'] ?></td>
+                            <td data-title="Nb exchange"><?= $player['CountExchange'] ?></td>
+                        </tr>
                         <?php } ?>
                         <tr>
                             <td class="number-box" data-title="#">2</td>
