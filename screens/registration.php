f525e869ddf96d2b6ff21fb1743fdcbdfee23c1d
Mae <fortunemaeva2001@gmail.com>
2023-03-14 00:35:37 +0100

    add register

--- screens/registration.php
+++ screens/registration.php
@@ -23,26 +23,29 @@ require_once("../includes/head.php") ?>
 <body>
     <?php require("../includes/navigation.php") ?>
     <div class="container">
+
         <h1 class="pb-2">Inscription</h1>
         <section>
-            <form action="" method="POST">
-                <div class="mb-3">
-                    <label for="nom" class="form-label">Nom</label>
-                    <input type="text" class="form-control" id="nom-register" name="nom" required>
-                </div>
-                <div class="mb-3">
-                    <label for="prenom" class="form-label">Prenom</label>
-                    <input type="text" class="form-control" id="prenom-register" name="prenom" required>
-                </div>
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="email-register" name="email" required>
-                </div>
-                <button type="submit" class="btn btn-primary">S'inscrire</button>
-            </form>
-
+            <div class="wrapper-sect">
+                <form action="" method="POST">
+                    <div class="mb-3">
+                        <label for="nom" class="form-label">Nom</label>
+                        <input type="text" class="form-control" id="nom-register" name="nom" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="prenom" class="form-label">Prenom</label>
+                        <input type="text" class="form-control" id="prenom-register" name="prenom" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email ENSC</label>
+                        <input type="email" class="form-control" id="email-register" name="email" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">S'inscrire</button>
+                </form>
+            </div>
         </section>
-        <div style="text-align: center;"> <img src="../assets/registration_player.gif" alt="meme-gif-Deal-or-not-deal">
+
+        <div style="text-align: center; margin-top: 2%;"> <img src="../assets/registration_player.gif" alt="meme-gif-Deal-or-not-deal">
         </div>
 
     </div>
