6b708e982383a5d6af214aadda35ad8d8879a299
Mae <fortunemaeva2001@gmail.com>
2023-03-15 00:08:02 +0100

    index

--- index.php
+++ index.php
@@ -21,15 +21,12 @@ require('sqlQuery.php');
     <link href="style/footer.css" rel="stylesheet">
 
     <!-- Bootstrap-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
 
     <!-- Font-awesome -->
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js"
-        integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous">
     </script>
 </head>
 
@@ -39,8 +36,7 @@ require('sqlQuery.php');
             <a href="apoal/registration.php"><img class="banniere-img" src="/assets/banniere.PNG" alt="banniere"></a>
         </div>
         <div class="banniere" id="banniere-mobile">
-            <a href="apoal/registration.php"><img class="banniere-img" src="/assets/banniere-mobile.PNG"
-                    alt="banniere"></a>
+            <a href="apoal/registration.php"><img class="banniere-img" src="/assets/banniere-mobile.PNG" alt="banniere"></a>
         </div>
         <div class="calendar">
             <div class="timeline">
@@ -115,6 +111,22 @@ require('sqlQuery.php');
                             <p class="title">Entertainment Law</p>
                             <p class="time">1PM - 4PM</p>
                         </div>
+                    </div>
+                </div>
+                <div class="day fri">
+                    <div class="date">
+                        <p class="date-num">13</p>
+                        <p class="date-day">Fri</p>
+                    </div>
+                    <div class="events">
+                    </div>
+                </div>
+                <div class="day fri">
+                    <div class="date">
+                        <p class="date-num">13</p>
+                        <p class="date-day">Fri</p>
+                    </div>
+                    <div class="events">
                     </div>
                 </div>
                 <div class="day fri">
