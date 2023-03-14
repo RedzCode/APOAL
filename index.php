fdc5086a5c34a7bea8f37c0e44e31e7e82f4ae0e
Mae <fortunemaeva2001@gmail.com>
2023-03-15 00:03:39 +0100

    index

--- index.php
+++ index.php
@@ -21,12 +21,15 @@ require('sqlQuery.php');
     <link href="style/footer.css" rel="stylesheet">
 
     <!-- Bootstrap-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
 
     <!-- Font-awesome -->
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js"
+        integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous">
     </script>
 </head>
 
@@ -36,7 +39,8 @@ require('sqlQuery.php');
             <a href="apoal/registration.php"><img class="banniere-img" src="/assets/banniere.PNG" alt="banniere"></a>
         </div>
         <div class="banniere" id="banniere-mobile">
-            <a href="apoal/registration.php"><img class="banniere-img" src="/assets/banniere.PNG" alt="banniere"></a>
+            <a href="apoal/registration.php"><img class="banniere-img" src="/assets/banniere-mobile.PNG"
+                    alt="banniere"></a>
         </div>
         <div class="calendar">
             <div class="timeline">
