1a4e020e2481e5b117292777eaa4a8a791e0fa12
Mae <fortunemaeva2001@gmail.com>
2023-03-14 16:02:01 +0100

    style

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,14 +1,16 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="../screens/players.php"><img class="logo-apoal" src=../assets/octo-apoal.png alt="logo_apoal" />
+        <a class="navbar-brand" href="../screens/players.php"><img class="logo-apoal" src=../assets/octo-apoal.png
+                alt="logo_apoal" />
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+            aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" href="players.php">Liste</a>
+                    <a class="nav-link" href="players.php">Liste</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="exchange.php">Echange</a>
@@ -24,12 +26,12 @@
     </div>
 </nav>
 <script>
-    $(function() {
-        $('.nav-link').each(function() {
-            if ($(this).prop('href') == window.location.href) {
-                $(this).addClass('active');
-                $(this).parents('li').addClass('active');
-            }
-        });
+$(function() {
+    $('.nav-link').each(function() {
+        if ($(this).prop('href') == window.location.href) {
+            $(this).addClass('active');
+            $(this).parents('li').addClass('active');
+        }
     });
+});
 </script>
