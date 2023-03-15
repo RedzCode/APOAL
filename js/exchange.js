81ea58c60573efff86f4b5559d16fbebed850472
Mae <fortunemaeva2001@gmail.com>
2023-03-15 11:58:16 +0100

    dropdown

--- js/exchange.js
+++ js/exchange.js
@@ -3,7 +3,11 @@ function checkMailsDifferent(){
     var mail2 = document.getElementById('myInput2');
 
     var btn = document.getElementById('btn-exchange');
-    if(mail1.value == mail2.value ){
+    console.log(mail1.value);
+    console.log(mail1.textContent);
+    console.log(mail2.value);
+    console.log(mail2.textContent);
+    if(mail1.textContent == mail2.textContent ){
         btn.disabled = true;
     }else{
         btn.disabled = false;
